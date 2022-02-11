49cc0b3b860ce2debeeb3323047c50870a4c0143
jf98uni <jf.botero@uniandes.edu.co>
2022-02-10 19:25:21 -0500

    Practica MVC parte 2

--- pong/main.js
+++ pong/main.js
@@ -13,13 +13,40 @@
         get elements (){
 
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
 
 
         }
     }
 })();
+
+(function(){
+
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+
+    }
+
+    self.Bar.prototype = {
+
+        down: function(){
+
+        },
+        up: function(){
+
+        }
+
+    }
+
+})();
+
 
 (function(){
     self.BoardView = function(canvas,board){
@@ -30,14 +57,44 @@
         this.ctx = canvas.getContext("2d");
 
     }
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for (var i = this.board.elements.length - 1; i>= 0 ; i--){
+                var el = this.board.elements[i];
+
+                draw(this.ctx,el)
+
+            };
+        }
+    }
+
+
+    function draw(ctx,element){
+
+        if (element !== null && element.hasOwnProperty("kind")){
+
+            switch(element.kind){
+                case "rectangle":
+
+                    ctx.fillRect(element.x,element.y,element.width,element.height);
+                    break;
+            }
+        }
+
+
+        
+    }
 })();
 
 window.addEventListener("load",main);
 
 function main(){
     var board = new Board(800,400);
+    var bar = new Bar(20,100,40,100,board)
     console.log(board);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas,board);
 
+    board_view.draw();
 }
