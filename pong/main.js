fb9810374221659ff499e328d67a6b14ebc50119
jf98uni <jf.botero@uniandes.edu.co>
2022-02-10 22:11:23 -0500

    MVC - 3 y comentarios

--- pong/main.js
+++ pong/main.js
@@ -1,4 +1,6 @@
 (function(){
+    // Esta funcion crea el tablero y sus elementos sin contamianr el scope general
+    // 
     self.Board = function (width,height){
 
         this.width = width;
@@ -9,6 +11,10 @@
         this.ball = null;    
     }
 
+
+    // coloca los metodos de la clase 
+    // JSON obtiene las barras y la pelota
+    // retorna todos los elementos 
     self.Board.prototype = {
         get elements (){
 
@@ -23,6 +29,8 @@
 
 (function(){
 
+    // esta es la encargada de crear las barras que suben y bajan
+
     self.Bar = function(x,y,width,height,board){
         this.x = x;
         this.y = y;
@@ -31,33 +39,43 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed =10;
 
     }
-
+    
+    // esta es la que da movimiento a las barras
     self.Bar.prototype = {
 
         down: function(){
 
+            this.y += this.speed;
+
         },
         up: function(){
-
+            this.x -= this.speed;
+        },
+        toString: function () {
+            return "x: " + this.x + " y: " + this.y;
         }
 
     }
 
 })();
 
-
+// es la vista, es lo qeu dibuja los elementos gracias al canvas.
 (function(){
     self.BoardView = function(canvas,board){
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
         this.board = board;
+        // es el objeto con el que podemos dibujar en JS 
         this.ctx = canvas.getContext("2d");
 
     }
 
+
+    // recorre los elementos en elements y los manda a dibujar 
     self.BoardView.prototype = {
         draw: function(){
             for (var i = this.board.elements.length - 1; i>= 0 ; i--){
@@ -69,7 +87,7 @@
         }
     }
 
-
+    // va a dibujar los elementos 
     function draw(ctx,element){
 
         if (element !== null && element.hasOwnProperty("kind")){
@@ -77,6 +95,7 @@
             switch(element.kind){
                 case "rectangle":
 
+                    // este rellena el rectangulo de la barra.
                     ctx.fillRect(element.x,element.y,element.width,element.height);
                     break;
             }
@@ -87,14 +106,30 @@
     }
 })();
 
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board)
+var bar = new Bar(735,100,40,100,board)
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas,board);
+
+document.addEventListener("keydown",function(ev){
+    
+    if(ev.keyCode == 38){
+        bar.up();
+    }
+    else if (ev.keyCode == 40){
+        bar.down();
+    }
+
+    console.log(bar.toString());
+    
+});
+
 window.addEventListener("load",main);
 
-function main(){
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100,board)
-    console.log(board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board);
+// ejecuta las clases o las llama a iniciarse
 
+function main(){
+    console.log(board);
     board_view.draw();
 }
